8425e7404e4a6a19a441e1e27f2890b7fecd09b7
Taimoor Khan <taimoor.m.k@live.com>
2018-04-10 10:36:18 +0500

    gitignore optimised

--- App.js
+++ App.js
@@ -1,58 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+// ########## Import Dependencies Here ##########
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// ########## Import Screens Here ##########
+import WalkThrough from './src/screens/WalkThrough';
 
-type Props = {};
-export default class App extends Component<Props> {
+// ########## Import Components Here ##########
+import { Drawer } from './src/config/router';
+
+class App extends Component {
+
+  constructor() {
+    super();
+  }
+
   render() {
+    let { isLoggedIn } = this.props
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      (isLoggedIn) ? <Drawer /> : <WalkThrough />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function mapStateToProps({launchAppReducer}) {
+  return {
+    isLoggedIn: launchAppReducer.isLoggedIn
+  }
+}
+
+export default connect(mapStateToProps, {})(App);
